2aebd378a7510b47f76183b2c57d42a261d4cb77
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-22 19:00:46 +0330

    feat (structure) ! : add api url to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiUrl: "https://x8ki-letl-twmt.n7.xano.io/api:bFKqZj63/",
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  apiUrl:"https://x8ki-letl-twmt.n7.xano.io/api:bFKqZj63/",
   production: false
 };
 
