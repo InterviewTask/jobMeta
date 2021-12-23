de6ab6a4c119887d2f8d3192158634dbf5c92833
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-23 09:33:08 +0330

    feat  : add lazy load application route path to main route file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./pages/applicant').then(m=>m.ApplicantModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
