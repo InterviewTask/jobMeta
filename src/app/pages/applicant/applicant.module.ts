dded05b046f2e1e823dc26a0738801b53585cdd1
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-23 12:18:06 +0330

    feat: create applicant list component

--- src/app/pages/applicant/applicant.module.ts
+++ src/app/pages/applicant/applicant.module.ts
@@ -5,13 +5,15 @@ import { ApplicantRoutingModule } from './applicant-routing.module';
 import { ApplicantComponent } from './applicant.component';
 import { ApplicantMasterLayoutComponent } from './layout/applicant-master-layout/applicant-master-layout.component';
 import { ApplicantNavLayoutComponent } from './layout/applicant-nav-layout/applicant-nav-layout.component';
+import { ApplicantListComponent } from './components/applicant-list/applicant-list.component';
 
 
 @NgModule({
   declarations: [
     ApplicantComponent,
     ApplicantMasterLayoutComponent,
-    ApplicantNavLayoutComponent
+    ApplicantNavLayoutComponent,
+    ApplicantListComponent
   ],
   imports: [
     CommonModule,
